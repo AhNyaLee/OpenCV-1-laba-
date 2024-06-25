ada3119ca7769490efd4621e77be075fe57687c1
AhNyaLee <jana.polya@yandex.ru>
2024-06-25 19:50:21 +0300

    Сделала 6 номер

--- nomer_6.py
+++ nomer_6.py
@@ -1,9 +1,28 @@
 import cv2
+def cross():
+   # Получаем размеры изображения
+   width = img.shape[1]
+   height = img.shape[0]
+   # Расчет координат прямоугольника(первого вертикального), чтобы он был по центру
+   rect_top_left = ((width - 50) // 2, (height + 10) // 2)
+   rect_bottom_right = (rect_top_left[0] + 30, rect_top_left[1] + 100)
+   # Рисуем прямоугольник (первого вертикального)
+   cv2.rectangle(img, rect_top_left, rect_bottom_right, (0, 0, 255), 1)
+   # Расчет координат прямоугольника(горизонтальный), чтобы он был по центру
+   rect_top_left = ((width - 200) // 2, (height - 50) // 2)
+   rect_bottom_right = (rect_top_left[0] + 200, rect_top_left[1] + 30)
+   # Рисуем прямоугольник (горизонтальный)
+   cv2.rectangle(img, rect_top_left, rect_bottom_right, (0, 0, 255), 1)
+   # Расчет координат прямоугольника(второго вертикального), чтобы он был по центру
+   rect_top_left = ((width - 50) // 2, (height - 250) // 2)
+   rect_bottom_right = (rect_top_left[0] + 30, rect_top_left[1] + 100)
+   # Рисуем прямоугольник (второго вертикального)
+   cv2.rectangle(img, rect_top_left, rect_bottom_right, (0, 0, 255), 1)
 cap=cv2.VideoCapture(0)
-ok, img = cap.read()#Если нужен один кадр
+#ok, img = cap.read()#Если нужен один кадр
 while (True):
-   # ok, img = cap.read()# если нам нужно  видео
-   cv2.rectangle(img, (15, 25), (200, 100), (0, 0, 255), 1)
+   ok, img = cap.read()# если нам нужно  видео
+   cross()
    cv2.imshow('img', img)
    if cv2.waitKey(1) & 0xFF == 27:
         break
